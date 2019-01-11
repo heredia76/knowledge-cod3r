cf8cab97c7c2f771b8705993cfeec8c1c181a7b8
Daniel Oliveira <danieliver2011@gmail.com>
2019-01-11 00:55:02 -0200

    ok

---
+++ versao-inicial/backend/migrations/20190111000201_create_table_categories.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('categories', table => {
+        table.increments('id')
+        table.string('name')
+        table.integer('parentId').references('id')
+            .inTable('categories')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('categories')
+};
